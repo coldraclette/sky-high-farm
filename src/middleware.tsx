a0c1dce4f2e2b5c260aafe79b3dcb5b9537e1ee3
Timo Tännler <timo.taennler@protonmail.com>
2023-11-10 18:17:43 +0200

    Remove unnecessary comments and simplify middleware logic.

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,9 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   const url = new URL(request.url);
 
-  // Check if the pathname is '/SUPPORT' and redirect to '/support'
   if (url.pathname === '/SUPPORT') {
-    // Construct an absolute URL for the redirect
     const destination = new URL('/support', request.url);
     return NextResponse.redirect(destination);
   }
